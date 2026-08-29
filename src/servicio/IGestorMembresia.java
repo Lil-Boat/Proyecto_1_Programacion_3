f60e8bf56ebed7081c444e5d8c06d51f11f3d605
Daniel Granados <gfallasmendez@gmail.com>
2026-08-28 19:34:20 -0600

    Commit: Se agrego IGestorMembresia

---
+++ src/servicio/IGestorMembresia.java
@@ -0,0 +1,24 @@
+package servicio;
+
+import modelo.Membresia;
+import modelo.Plan;
+import modelo.Usuario;
+
+
+public interface IGestorMembresia {
+
+    //Asigna un plan a un usuario y cobra el precio del plan
+    //Retorna la membresia creada o actualizada
+    Membresia asignarPlanYCobrar(Usuario usuario, Plan plan);
+
+
+    //Registra el pago sobre la membresia del usuario
+    void registrarPago(Usuario usuario);
+
+    //Busca la membresia de un usuario
+    //Retorna la membresia encontrada o null si no tiene
+    Membresia buscarMembresiaPorUsuario(Usuario usuario);
+
+    //Elimina la memebresia asociada a un usuario, si existe
+    void eliminarMembresia(Usuario usuario);
+}
